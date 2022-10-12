e9dcd3e32511659ddea6a4ba449bb725a9c3258d
Caio Anderson <110497351+caioandersonf@users.noreply.github.com>
2022-10-12 18:04:45 -0300

    Update cadastra.php

--- cadastra.php
+++ cadastra.php
@@ -8,10 +8,12 @@ $dbname = "bancodedados";
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = $_POST['senha'];
 
 $result_usuario = "INSERT INTO usuarios (email, senha) VALUES ('$email','$password')";
 
 $resultado_usuario = mysqli_query($conn,$result_usuario);
+
+header("location:index.php");
 
 ?>
