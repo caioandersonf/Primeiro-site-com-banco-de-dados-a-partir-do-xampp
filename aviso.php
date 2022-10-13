58016ab5ee60239d270015582b65965d3310be53
Caio Anderson <110497351+caioandersonf@users.noreply.github.com>
2022-10-13 11:51:59 -0300

    Add files via upload

---
+++ aviso.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Aviso</title>
+        <script type="text/javascript">
+            alert("Email já cadastrado!");
+            window.location.href="index.php";
+        </script>
+    </head>
+    <body>
+    </body>
+</html>
